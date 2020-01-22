7ab03694644c6b72dd472287190fc77b95d2c426
Sarani1612 <sara.larsen1612@gmail.com>
2020-01-22 09:58:46 +0000

    updated app.py

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def delete_task(task_id):
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
